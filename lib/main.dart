bfa162d02181146a42ff79b24d412761c9a0bfc5
김상연 <tallpizza@danyeon.local>
2023-05-15 23:29:20 +0900

    드래그 가능한 범위를 박스로 지정하고 테두리 표시하기 #10

--- lib/main.dart
+++ lib/main.dart
@@ -41,63 +41,77 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       appBar: AppBar(
         title: const Text('Sample Code'),
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Positioned(
-            top: _top,
-            left: _left,
-            child: GestureDetector(
-              onPanUpdate: (details) {
-                setState(() {
-                  _top += details.delta.dy;
-                  _left += details.delta.dx;
-                });
-              },
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: const BoxDecoration(
-                  color: Colors.lightBlue,
-                  shape: BoxShape.circle,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ValueListenableBuilder<String>(
-                        builder: (BuildContext context, String value,
-                            Widget? child) {
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text("$value%",
-                                  style: const TextStyle(
-                                      color: Colors.deepPurpleAccent,
-                                      fontSize: 35))
-                            ],
-                          );
-                        },
-                        valueListenable: mqttHandler.humi),
-                    ValueListenableBuilder<String>(
-                        builder: (BuildContext context, String value,
-                            Widget? child) {
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text("$value °C",
-                                  style: const TextStyle(
-                                      color: Colors.deepPurpleAccent,
-                                      fontSize: 35))
-                            ],
-                          );
-                        },
-                        valueListenable: mqttHandler.temp),
-                  ],
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 300,
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Positioned(
+              top: _top,
+              left: _left,
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  setState(() {
+                    if (_top + details.delta.dy < 100 &&
+                        _top + details.delta.dy > 0) {
+                      _top += details.delta.dy;
+                    }
+                    if (_left + details.delta.dx <
+                            MediaQuery.of(context).size.width - 220 &&
+                        _left + details.delta.dx > 0) {
+                      _left += details.delta.dx;
+                    }
+                  });
+                },
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: Colors.lightBlue,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ValueListenableBuilder<String>(
+                          builder: (BuildContext context, String value,
+                              Widget? child) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("$value%",
+                                    style: const TextStyle(
+                                        color: Colors.deepPurpleAccent,
+                                        fontSize: 35))
+                              ],
+                            );
+                          },
+                          valueListenable: mqttHandler.humi),
+                      ValueListenableBuilder<String>(
+                          builder: (BuildContext context, String value,
+                              Widget? child) {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text("$value °C",
+                                    style: const TextStyle(
+                                        color: Colors.deepPurpleAccent,
+                                        fontSize: 35))
+                              ],
+                            );
+                          },
+                          valueListenable: mqttHandler.temp),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => setState(() => _count++),
